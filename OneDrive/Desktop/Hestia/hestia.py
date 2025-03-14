2473143dc7b355bb5b62c5600c289e0a91b2da74
Gianna Casselli <tcassell@uoguelph.ca>
2025-03-13 21:43:03 -0400

    Adding inital daily weather data

--- OneDrive/Desktop/Hestia/hestia.py
+++ OneDrive/Desktop/Hestia/hestia.py
@@ -17,7 +17,7 @@ df.columns = df.columns.str.lower()
 # print("\nSummary Statistics:")
 # print(df.describe())
 
-# Remove missing values for relevant columns
+#Remove missing values for relevant columns
 df = df.dropna(subset=["relative_humidity", "wind_speed", "wind_direction", "current_size"])
 
 #Scatter Plot: Humidity vs. Fire Size
@@ -59,3 +59,20 @@ plt.show()
 # plt.xlabel("Year")
 # plt.ylabel("Number of Wildfires")
 # plt.show()
+
+# Gather data of daily weather conditions in Alberta
+# Wildfire data uses locations based in longitude and latitude
+# Need to conver location weather data to long/lat coordinates and merge with wildfire csv
+# Fort McMurray A, Fort McMurray: Lat = 56.72667, Lon = -111.38083 - Data acquired for 2016 - 2025
+# Adair Auto, Zama City: Lat = 59.15222, Lon = -118.69250
+# Rainbow Lake Auto, Rainbow Lake: Lat = 58.50444, Lon = -119.40028 - Data acquired for 2020 - 2025
+# Shell Hamburg, Chinchaga Wildland Provincial Park: Lat = 57.15000, Lon = -119.56667 - Data acquired for 2016 - 2025
+# Entrance Auto, Old Entrance: Lat = 53.36667, Lon = -117.71889 - Data acquired for 2016 - 2025
+# Wabasca Auto, Wabasca: Lat = 55.96666, Lon = -113.85000 - Data acquired for 2016 - 2025
+# Loon River Auto, Loon River Airport: Lat = 57.14200, Lon = 115.07533 - Data acquired for 2016 - 2025
+# Foggy Mountain Auto, John D'Or Prairie: Lat = 58.50100, Lon = -115.14280
+# Red Earth Auto, Red Earth Creek: Lat = 56.54972, Lon = -115.28450
+# Bovine Creek Auto, Mariana Lake: Lat = 55.95216, Lon = -112.01667
+# Whitemud Creek AGCM, New Fish Creek: Lat = 55.29000, Lon = -117.25450
+# Edmonto Stony Plain CS, Stony Plain: Lat = 53.53343, Lon = -114.00205 - Data acquired for 2016 - 2025
+# Canmore Civic Centre, Canmore, Lat = 51.08340, Lon = -115.3521
